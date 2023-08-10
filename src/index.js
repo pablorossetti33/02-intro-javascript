441407d11e04da2b826aa604461fa5579d333fed
Pablo Rossetti <prensap.ross@gmail.com>
2023-08-10 11:19:49 -0300

    second commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 
 
-console.log("Intro Javascript Avanzado");
+//Variables y Constantes.
+
+const nombre = "Pablo";
+const apellido = "Rossetti";
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado)
+
+if(true){
+    let valorDado = 6;
+
+    console.log(valorDado)
+}
+
+console.log(valorDado);
